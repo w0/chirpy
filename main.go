fc80fa48e8b519be6d767c276f995ef10405954e
w0 <33212583+w0@users.noreply.github.com>
2024-12-02 18:55:59 -0600

    move to /admin

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
 
 	mux.HandleFunc("GET /api/healthz", handlerHealthz)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerResetMetrics)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetMetrics)
 
 	server := http.Server{
 		Handler: mux,
@@ -49,10 +49,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
-	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	hits := fmt.Sprintf(`<html>
+						<body>
+							<h1>Welcome, Chirpy Admin</h1>
+							<p>Chirpy has been visited %d times!</p>
+						</body>
+						</html>`,
+		cfg.fileserverHits.Load())
+
 	io.WriteString(w, hits)
 }
 
